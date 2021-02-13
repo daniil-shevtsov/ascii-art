36e077c784a3fcbd4bd8d2a4e0fb3dc33756f835
Daniil Shevtsov <me@daniilshevtsov.com>
2021-02-13 13:45:32 +0300

    draw char chess board

--- app/src/main/java/shevtsov/daniil/asciiart/main/view/AsciiView.kt
+++ app/src/main/java/shevtsov/daniil/asciiart/main/view/AsciiView.kt
@@ -2,17 +2,57 @@ package shevtsov.daniil.asciiart.main.view
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Typeface
 import android.util.AttributeSet
-import android.view.View
+import android.util.Log
+import androidx.appcompat.widget.AppCompatTextView
 
 class AsciiView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    private val gridCreator = GridCreator()
+
+    init {
+        textScaleX = 1.5f;
+        textSize = 20f;
+        typeface = Typeface.MONOSPACE;
+        setBackgroundColor(Color.BLACK)
+        setTextColor(Color.WHITE)
+
+        val grid = gridCreator.createGrid(width = GRID_WIDTH, height = GRID_HEIGHT)
+
+        val pixelSize = width.toFloat() / GRID_WIDTH
+
+        Log.d("AsciiView", "count: ${grid.size} width: $width size: $pixelSize")
+
+        text = grid
+            .windowed(size = GRID_WIDTH, step = GRID_WIDTH)
+            .joinToString(separator = "\n") { gridCoordinates ->
+                gridCoordinates.joinToString(separator = "") { gridCoordinate ->
+                    if (when {
+                            gridCoordinate.y % 2 == 0 -> gridCoordinate.x % 2 == 0
+                            else -> gridCoordinate.x % 2 != 0
+                        }
+                    ) {
+                        "1"
+                    } else {
+                        "0"
+                    }
+                }
+            }
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
+    }
+
+    private companion object {
+        const val GRID_WIDTH = 8
+        const val GRID_HEIGHT = 8
     }
 }
